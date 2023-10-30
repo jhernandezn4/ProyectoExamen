1e855a01e14b8161423bd2e3849ea997efed9ce2
Jorge Asus <jorgehn1995@gmail.com>
2023-10-30 07:50:48 -0600

    Se finalizo el proyecto

--- src/main/java/lerniz/Main.java
+++ src/main/java/lerniz/Main.java
@@ -1,19 +1,35 @@
 package lerniz;
 
+import lerniz.DAO.UsuariosDAO;
+import lerniz.DTO.UsuariosDTO;
+import lerniz.Vistas.Login;
+import org.mindrot.jbcrypt.BCrypt;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
+
     public static void main(String[] args) {
-        // Press Alt+Intro with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        // Press Mayús+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        Login login = new Login();
+        login.pack();
+        login.setVisible(true);
 
-            // Press Mayús+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+    }
+    public static void crearUsuarioInicial(){
+        //SE ENCRIPTA LA CONTRASEÑA
+        String password = "1234";
+        String hashedPassword = BCrypt.hashpw(password, BCrypt.gensalt());
+
+        UsuariosDTO usuario= new UsuariosDTO();
+        usuario.setUsuario("jorge");
+        usuario.setNombre("Jorge Hernandez");
+        usuario.setPassword(hashedPassword);
+
+        UsuariosDAO DBUsuario= new UsuariosDAO();
+        DBUsuario.crear(usuario);
+        System.out.println("usuario creado");
+
+
     }
 }
--- src/main/java/lerniz/views/Login.java
+++
@@ -1,9 +0,0 @@
-package lerniz.views;
-
-import javax.swing.*;
-
-public class Login {
-    private JTextField textField1;
-    private JTextField textField2;
-    private JButton button1;
-}
